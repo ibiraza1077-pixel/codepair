370921173797108a0a2c3c44c80ffc75e6756e57
Ibrahim <ibiraza1077@gmail.com>
2026-02-18 11:23:32 +0000

    Fix Home.tsx to use production API URL

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Code2, Users, Zap } from 'lucide-react';
 
-const API_URL = 'http://localhost:5000';
+const API_URL = 'https://hearty-abundance-production.up.railway.app';
 
 function Home() {
   const [username, setUsername] = useState('');
